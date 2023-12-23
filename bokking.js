12ca106924aa29aec7f81fed15e94c7631778e27
Alobin90 <razmaev.leha@mail.ru>
2023-12-23 04:52:22 +0300

    upload

---
+++ bokking.js
@@ -0,0 +1,29 @@
+function clickH(){
+	alert("Бронирование прошло успешно. Данные отправлены на почту.");
+	window.open('https://mail.ru/', '_blank');
+}
+
+function fun1(){
+	document.getElementById('fun1').innerHTML = 'Комната 2D';
+	document.getElementById('fun1').style.opacity = '100%';
+}
+
+function fun2(){
+	document.getElementById('fun2').innerHTML = '20.08.23';
+	document.getElementById('fun2').style.opacity = '100%';
+}
+
+function fun3(){
+	document.getElementById('fun3').innerHTML = '16:00';
+	document.getElementById('fun3').style.opacity = '100%';
+}
+
+function fun4(){
+	document.getElementById('fun4').innerHTML = '36 часов';
+	document.getElementById('fun4').style.opacity = '100%';
+}
+
+function funBB(){
+	document.getElementById('funBB').innerHTML = '+7-800-555-35-35';
+	document.getElementById('funBB').style.opacity = '100%';
+}
